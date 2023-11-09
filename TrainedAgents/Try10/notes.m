7e402ab7341988ab688a25cf343ba5f4b6f68b13
Ryan Schultz <rjs10@ualberta.ca>
2023-11-09 13:15:00 +0100

    Add files via upload

---
+++ TrainedAgents/Try10/notes.m
@@ -0,0 +1,11 @@
+%%% Notes to myself and future readers about lessons learned.
+% Try 10 (25 July 2023).
+
+% The training process is still straightforward, with a bit of 
+% underestimation  bias.  The agent still isn't learning too much though.  
+% This entire session essentially just taught it the correct first move.  
+% I'm assuming this is because the agent's having a hard time finding the 
+% sparse reward for the narrow band I put on close velocties between steps.
+% There's still room to further explore reward shaping and greedier 
+% discount factors, I think.
+% 
