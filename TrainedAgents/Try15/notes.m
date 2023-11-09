ef9556d0f0542d6cac3329a34ed51ce59708e323
Ryan Schultz <rjs10@ualberta.ca>
2023-11-09 13:26:05 +0100

    Add files via upload

---
+++ TrainedAgents/Try15/notes.m
@@ -0,0 +1,15 @@
+%%% Notes to myself and future readers about lessons learned.
+% Try 15 (4 Sept 2023).
+
+% Let's try this again.  This time I'm being a little a tiny bit more 
+% conservative with how I decrease the noise plateaus - otherwise, 
+% everything else is the same as Try 13.  This idea is based on the 
+% observation that I was able to get past Try13's p2 crash before when 
+% using Matlab's noise resetting bug/feature.
+% 
+% Using a p2 noise plateau of 0.03 doesn't seem to have fixed that issue.
+% I need to think more seriously about stabilizing the learning process,
+% apparently.
+% 
+% 
+
