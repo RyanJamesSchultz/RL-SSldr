50d994d5505f057e915cd553a648e8ed37229f93
Ryan Schultz <rjs10@ualberta.ca>
2023-11-09 13:07:42 +0100

    Add files via upload

---
+++ TrainedAgents/Try2/notes.m
@@ -0,0 +1,12 @@
+%%% Notes to myself and future readers about lessons learned.
+% Try 2 (20 June 2023).
+
+% This was a first attempt at training an agent to just do Step 2.  The 
+% agent has failed to learn much, even after a million training episodes.  
+% I also see a significant Q overestimation bias.
+% 
+% My apologies, I didn't think to include the script_DDPG.m file 
+% (or even these notes) when I first started working on this project.  Some 
+% of the parameters set can be found by looking at the agent class, within 
+% the *.mat files.
+% 
