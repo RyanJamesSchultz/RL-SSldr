7c8d4df909f36502d03d687161ccad64b93b14f9
Ryan Schultz <rjs10@ualberta.ca>
2023-11-09 13:10:25 +0100

    Add files via upload

---
+++ TrainedAgents/Try4/notes.m
@@ -0,0 +1,6 @@
+%%% Notes to myself and future readers about lessons learned.
+% Try 4 (7 July 2023).
+
+% This was one of the more egregious examples of catastrophic forgetting.
+% It showcases how brittle the results can be to hyperparameters.
+% 
