06a55d7a3d3d94182cc220ad98cf1bc4d866c873
Ryan Schultz <rjs10@ualberta.ca>
2023-11-09 13:13:44 +0100

    Add files via upload

---
+++ TrainedAgents/Try8/notes.m
@@ -0,0 +1,11 @@
+%%% Notes to myself and future readers about lessons learned.
+% Try 8 (21 July 2023).
+
+% I'm getting closer to straighforward learning.  This mostly seems to
+% have been a function of slowing down learning rates and noise decay
+% rates.  Said another way, exploring longer and learning more slowly.  
+% This avoids acting on catastrophic choices, by sampling bad examples 
+% better and not changing to poor actors as quickly.  Still ended up being 
+% a problem eventually though.
+% 
+
